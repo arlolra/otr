db6b409100c28774fe4dc683c87922f94a2fc53a
William Budington <bill.budington@gmail.com>
2012-07-29 16:58:14 -0700

    merging tests
    Signed-off-by: Arlo Breault <arlolra@gmail.com>

--- test/spec/unit/dsa.js
+++ test/spec/unit/dsa.js
@@ -1,12 +1,12 @@
 var assert = require('assert')
-  , HLP = require('../helpers.js')
-  , DSA = require('../dsa.js')
-  , BigInt = require('../vendor/bigint.js')
+  , HLP = require('../../../helpers.js')
+  , DSA = require('../../../dsa.js')
+  , BigInt = require('../../../vendor/bigint.js')
 
 describe('DSA', function(){
   var key;
   before(function(){
-    key = new dsa.Key()
+    key = new DSA.Key()
     console.log('p: ' + BigInt.bigInt2str(key.p, 10))
     console.log('q: ' + BigInt.bigInt2str(key.q, 10))
     console.log('x: ' + BigInt.bigInt2str(key.x, 10))
@@ -16,7 +16,7 @@ describe('DSA', function(){
 
   it('should generate a key with q > 2^159 and q < 2^160', function(){
     assert.ok(
-            hlp.between(key.q, hlp.twotothe(159), hlp.twotothe(160)),'In between.')
+            HLP.between(key.q, HLP.twotothe(159), HLP.twotothe(160)),'In between.')
   });
 
   it('should generate a key with q being a multiple of q', function(){
@@ -30,11 +30,11 @@ describe('DSA', function(){
  
   it('should verify a valid signature', function(){
     var s = key.sign('abc')
-    assert.equal(1, dsa.verify(key, 'abc', s[0], s[1]), 'Verify signed message.')
+    assert.equal(1, DSA.verify(key, 'abc', s[0], s[1]), 'Verify signed message.')
   });
 
   it('should parse a given public key into the correct parameters', function(){
-      var par = dsa.parsePublic(key.packPublic())
+      var par = DSA.parsePublic(key.packPublic())
       assert.ok(BigInt.equals(key.p, par.p), 'Pees are good.')
       assert.ok(BigInt.equals(key.q, par.q), 'Qs.')
       assert.ok(BigInt.equals(key.g, par.g), 'Gs.')
