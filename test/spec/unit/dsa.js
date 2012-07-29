98e4bf69d0bcfcd2ea2955115ea6c77deec2c2ad
William Budington <bill.budington@gmail.com>
2012-07-29 16:21:21 -0700

    wrapping tests in mocha test structure
    Signed-off-by: Arlo Breault <arlolra@gmail.com>

--- test/spec/unit/dsa.js
+++ test/spec/unit/dsa.js
@@ -3,28 +3,41 @@ var assert = require('assert')
   , DSA = require('../dsa.js')
   , BigInt = require('../vendor/bigint.js')
 
-var key = new DSA.Key()
+describe('DSA', function(){
+  var key;
+  before(function(){
+    key = new dsa.Key()
+    console.log('p: ' + BigInt.bigInt2str(key.p, 10))
+    console.log('q: ' + BigInt.bigInt2str(key.q, 10))
+    console.log('x: ' + BigInt.bigInt2str(key.x, 10))
+    console.log('g: ' + BigInt.bigInt2str(key.g, 10))
+    console.log('counter: ' + key.counter)
+  });
 
-assert.ok(
-	HLP.between(key.q, HLP.twotothe(159), HLP.twotothe(160)),'In between.')
+  it('should generate a key with q > 2^159 and q < 2^160', function(){
+    assert.ok(
+            hlp.between(key.q, hlp.twotothe(159), hlp.twotothe(160)),'In between.')
+  });
 
-var quotient = BigInt.str2bigInt('0', 10, BigInt.bitSize(key.p))
-  , remainder = BigInt.str2bigInt('0', 10, BigInt.bitSize(key.p))
-var p_minus = BigInt.sub(key.p, BigInt.str2bigInt('1', 10))
-BigInt.divide_(p_minus, key.q, quotient, remainder)
-assert.ok(BigInt.isZero(remainder), 'Multiple.')
+  it('should generate a key with q being a multiple of q', function(){
+    var quotient = BigInt.str2bigInt('0', 10, BigInt.bitSize(key.p))
+      , remainder = BigInt.str2bigInt('0', 10, BigInt.bitSize(key.p))
+    var p_minus = BigInt.sub(key.p, BigInt.str2bigInt('1', 10))
+    BigInt.divide_(p_minus, key.q, quotient, remainder)
+    assert.ok(BigInt.isZero(remainder), 'Multiple.')
+  });
 
-console.log('p: ' + BigInt.bigInt2str(key.p, 10))
-console.log('q: ' + BigInt.bigInt2str(key.q, 10))
-console.log('x: ' + BigInt.bigInt2str(key.x, 10))
-console.log('g: ' + BigInt.bigInt2str(key.g, 10))
-console.log('counter: ' + key.counter)
+ 
+  it('should verify a valid signature', function(){
+    var s = key.sign('abc')
+    assert.equal(1, dsa.verify(key, 'abc', s[0], s[1]), 'Verify signed message.')
+  });
 
-var s = key.sign('abc')
-assert.equal(1, DSA.verify(key, 'abc', s[0], s[1]), 'Verify signed message.')
-
-var par = DSA.parsePublic(key.packPublic())
-assert.ok(BigInt.equals(key.p, par.p), 'Pees are good.')
-assert.ok(BigInt.equals(key.q, par.q), 'Qs.')
-assert.ok(BigInt.equals(key.g, par.g), 'Gs.')
-assert.ok(BigInt.equals(key.y, par.y), 'Ys.')
+  it('should parse a given public key into the correct parameters', function(){
+      var par = dsa.parsePublic(key.packPublic())
+      assert.ok(BigInt.equals(key.p, par.p), 'Pees are good.')
+      assert.ok(BigInt.equals(key.q, par.q), 'Qs.')
+      assert.ok(BigInt.equals(key.g, par.g), 'Gs.')
+      assert.ok(BigInt.equals(key.y, par.y), 'Ys.')
+  });
+});
