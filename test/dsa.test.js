5552a54fed37e32ba04fe5dffef4fe170b971262
William Budington <bill.budington@gmail.com>
2012-07-29 15:08:01 -0700

    moving tests into test/spec/unit
    Signed-off-by: Arlo Breault <arlolra@gmail.com>

--- test/dsa.test.js
+++ test/dsa.test.js
@@ -5,6 +5,17 @@ var assert = require('assert')
 
 var key = new DSA.Key()
 
+describe('DSA', function(){
+  var key;
+  before(function(){
+    key = new DSA.Key()
+    console.log('p: ' + BigInt.bigInt2str(key.p, 10))
+    console.log('q: ' + BigInt.bigInt2str(key.q, 10))
+    console.log('x: ' + BigInt.bigInt2str(key.x, 10))
+    console.log('g: ' + BigInt.bigInt2str(key.g, 10))
+    console.log('counter: ' + key.counter)
+  });
+
 assert.ok(
 	HLP.between(key.q, HLP.twotothe(159), HLP.twotothe(160)),'In between.')
 
@@ -13,12 +24,6 @@ var quotient = BigInt.str2bigInt('0', 10, BigInt.bitSize(key.p))
 var p_minus = BigInt.sub(key.p, BigInt.str2bigInt('1', 10))
 BigInt.divide_(p_minus, key.q, quotient, remainder)
 assert.ok(BigInt.isZero(remainder), 'Multiple.')
-
-console.log('p: ' + BigInt.bigInt2str(key.p, 10))
-console.log('q: ' + BigInt.bigInt2str(key.q, 10))
-console.log('x: ' + BigInt.bigInt2str(key.x, 10))
-console.log('g: ' + BigInt.bigInt2str(key.g, 10))
-console.log('counter: ' + key.counter)
 
 var s = key.sign('abc')
 assert.equal(1, DSA.verify(key, 'abc', s[0], s[1]), 'Verify signed message.')
--- test/parse.test.js
+++ test/parse.test.js
@@ -1,61 +1,80 @@
 var assert = require('assert')
   , OTR = function () { this.versions = {} }  // just a constructor
-  , P = require('../parse.js')
+  , P = require('../../../parse.js')
 
-var otr = new OTR()
-assert.equal('hi', P.parseMsg(otr, 'hi'), 'Hi.')
-assert.equal('hi?OTR', P.parseMsg(otr, 'hi?OTR'), 'OTR!')
 
-var MSGFRAG1 = '?OTR,1,3,?OTR:AAIKAAAAAQAAAAEAAADAVf3Ei72ZgFeKqWvLMnuVPVCwxktsOZ1QdjeLp6jn62mCVtlY9nS6sRkecpjuLYHRxyTdRu2iEVtSsjZqK55ovZ35SfkOPHeFYa9BIuxWi9djHMVKQ8KOVGAVLibjZ6P8LreDSKtWDv9YQjIEnkwFVGCPfpBq2SX4VTQfJAQXHggR8izKxPvluXUdG9rIPh4cac98++VLdIuFMiEXjUIoTX2rEzunaCLMy0VIfowlRsgsKGrwhCCv7hBWyglbzwz+AAAAAAAAAAQAAAF2SOr,'
-var MSGFRAG2 = '?OTR,2,3,JvPUerB9mtf4bqQDFthfoz/XepysnYuReHHEXKe+BFkaEoMNGiBl4TCLZx72DvmZwKCewWRH1+W66ggrXKw2VdVl+vLsmzxNyWChGLfBTL5/3SUF09BfmCEl03Ckk7htAgyAQcBf90RJznZndv7HwVAi3syupi0sQDdOKNPyObR5FRtqyqudttWmSdmGCGFcZ/fZqxQNsHB8QuYaBiGL7CDusES+wwfn8Q7BGtoJzOPDDx6KyIyox/flPx2DZDJIZrMz9b0V70a9kqKLo/wcGhvHO6coCyMxenBAacLJ1DiI,'
-var MSGFRAG3 = '?OTR,3,3,NLKoYOoJTM7zcxsGnvCxaDZCvsmjx3j8Yc5r3i3ylllCQH2/lpr/xCvXFarGtG7+wts+UqstS9SThLBQ9Ojq4oPsX7HBHKvq19XU3/ChIgWMy+bczc5gpkC/eLAIGfJ0D5DJsl68vMXSmCoFK0HTwzzNa7lnZK4IutYPBNBCv0pWORQqDpskEz96YOGyB8+gtpFgCrkuV1bSB9SRVmEBfDtKPQFhKowAAAAA=.,'
-var MSGFRAG = '?OTR:AAIKAAAAAQAAAAEAAADAVf3Ei72ZgFeKqWvLMnuVPVCwxktsOZ1QdjeLp6jn62mCVtlY9nS6sRkecpjuLYHRxyTdRu2iEVtSsjZqK55ovZ35SfkOPHeFYa9BIuxWi9djHMVKQ8KOVGAVLibjZ6P8LreDSKtWDv9YQjIEnkwFVGCPfpBq2SX4VTQfJAQXHggR8izKxPvluXUdG9rIPh4cac98++VLdIuFMiEXjUIoTX2rEzunaCLMy0VIfowlRsgsKGrwhCCv7hBWyglbzwz+AAAAAAAAAAQAAAF2SOrJvPUerB9mtf4bqQDFthfoz/XepysnYuReHHEXKe+BFkaEoMNGiBl4TCLZx72DvmZwKCewWRH1+W66ggrXKw2VdVl+vLsmzxNyWChGLfBTL5/3SUF09BfmCEl03Ckk7htAgyAQcBf90RJznZndv7HwVAi3syupi0sQDdOKNPyObR5FRtqyqudttWmSdmGCGFcZ/fZqxQNsHB8QuYaBiGL7CDusES+wwfn8Q7BGtoJzOPDDx6KyIyox/flPx2DZDJIZrMz9b0V70a9kqKLo/wcGhvHO6coCyMxenBAacLJ1DiINLKoYOoJTM7zcxsGnvCxaDZCvsmjx3j8Yc5r3i3ylllCQH2/lpr/xCvXFarGtG7+wts+UqstS9SThLBQ9Ojq4oPsX7HBHKvq19XU3/ChIgWMy+bczc5gpkC/eLAIGfJ0D5DJsl68vMXSmCoFK0HTwzzNa7lnZK4IutYPBNBCv0pWORQqDpskEz96YOGyB8+gtpFgCrkuV1bSB9SRVmEBfDtKPQFhKowAAAAA=.'
+describe('OTR', function(){
+  var otr;
 
-assert.equal('', P.parseMsg(otr, MSGFRAG1), 'Message fragment 1.')
-assert.equal('', P.parseMsg(otr, MSGFRAG2), 'Message fragment 2.')
-var three = P.parseMsg(otr, MSGFRAG3)
-assert.equal(three, P.parseMsg(otr, MSGFRAG), 'Message fragment.')
-assert.equal('OTR', three, 'Message fragment 3.')
+  beforeEach(function(){
+    otr = new OTR();
+  });
 
-otr = new OTR()
-P.parseMsg(otr, '?OTR?')
-assert.equal(1, Object.keys(otr.versions).length, 'version 1')
-assert.ok(otr.versions['1'], 'version 1')
+  it('should detect message fragments', function(){
+    assert.equal('hi', P.parseMsg(otr, 'hi'), 'Hi.')
+    assert.equal('hi?OTR', P.parseMsg(otr, 'hi?OTR'), 'OTR!')
 
-otr = new OTR()
-P.parseMsg(otr, '?OTRv2?')
-assert.equal(1, Object.keys(otr.versions).length, 'version 2')
-assert.ok(otr.versions['2'], 'version 2')
+    var MSGFRAG1 = '?OTR,1,3,?OTR:AAIKAAAAAQAAAAEAAADAVf3Ei72ZgFeKqWvLMnuVPVCwxktsOZ1QdjeLp6jn62mCVtlY9nS6sRkecpjuLYHRxyTdRu2iEVtSsjZqK55ovZ35SfkOPHeFYa9BIuxWi9djHMVKQ8KOVGAVLibjZ6P8LreDSKtWDv9YQjIEnkwFVGCPfpBq2SX4VTQfJAQXHggR8izKxPvluXUdG9rIPh4cac98++VLdIuFMiEXjUIoTX2rEzunaCLMy0VIfowlRsgsKGrwhCCv7hBWyglbzwz+AAAAAAAAAAQAAAF2SOr,'
+    var MSGFRAG2 = '?OTR,2,3,JvPUerB9mtf4bqQDFthfoz/XepysnYuReHHEXKe+BFkaEoMNGiBl4TCLZx72DvmZwKCewWRH1+W66ggrXKw2VdVl+vLsmzxNyWChGLfBTL5/3SUF09BfmCEl03Ckk7htAgyAQcBf90RJznZndv7HwVAi3syupi0sQDdOKNPyObR5FRtqyqudttWmSdmGCGFcZ/fZqxQNsHB8QuYaBiGL7CDusES+wwfn8Q7BGtoJzOPDDx6KyIyox/flPx2DZDJIZrMz9b0V70a9kqKLo/wcGhvHO6coCyMxenBAacLJ1DiI,'
+    var MSGFRAG3 = '?OTR,3,3,NLKoYOoJTM7zcxsGnvCxaDZCvsmjx3j8Yc5r3i3ylllCQH2/lpr/xCvXFarGtG7+wts+UqstS9SThLBQ9Ojq4oPsX7HBHKvq19XU3/ChIgWMy+bczc5gpkC/eLAIGfJ0D5DJsl68vMXSmCoFK0HTwzzNa7lnZK4IutYPBNBCv0pWORQqDpskEz96YOGyB8+gtpFgCrkuV1bSB9SRVmEBfDtKPQFhKowAAAAA=.,'
+    var MSGFRAG = '?OTR:AAIKAAAAAQAAAAEAAADAVf3Ei72ZgFeKqWvLMnuVPVCwxktsOZ1QdjeLp6jn62mCVtlY9nS6sRkecpjuLYHRxyTdRu2iEVtSsjZqK55ovZ35SfkOPHeFYa9BIuxWi9djHMVKQ8KOVGAVLibjZ6P8LreDSKtWDv9YQjIEnkwFVGCPfpBq2SX4VTQfJAQXHggR8izKxPvluXUdG9rIPh4cac98++VLdIuFMiEXjUIoTX2rEzunaCLMy0VIfowlRsgsKGrwhCCv7hBWyglbzwz+AAAAAAAAAAQAAAF2SOrJvPUerB9mtf4bqQDFthfoz/XepysnYuReHHEXKe+BFkaEoMNGiBl4TCLZx72DvmZwKCewWRH1+W66ggrXKw2VdVl+vLsmzxNyWChGLfBTL5/3SUF09BfmCEl03Ckk7htAgyAQcBf90RJznZndv7HwVAi3syupi0sQDdOKNPyObR5FRtqyqudttWmSdmGCGFcZ/fZqxQNsHB8QuYaBiGL7CDusES+wwfn8Q7BGtoJzOPDDx6KyIyox/flPx2DZDJIZrMz9b0V70a9kqKLo/wcGhvHO6coCyMxenBAacLJ1DiINLKoYOoJTM7zcxsGnvCxaDZCvsmjx3j8Yc5r3i3ylllCQH2/lpr/xCvXFarGtG7+wts+UqstS9SThLBQ9Ojq4oPsX7HBHKvq19XU3/ChIgWMy+bczc5gpkC/eLAIGfJ0D5DJsl68vMXSmCoFK0HTwzzNa7lnZK4IutYPBNBCv0pWORQqDpskEz96YOGyB8+gtpFgCrkuV1bSB9SRVmEBfDtKPQFhKowAAAAA=.'
 
-otr = new OTR()
-P.parseMsg(otr, '?OTR?v2?')
-assert.equal(2, Object.keys(otr.versions).length, 'version 1 & 2')
-assert.ok(otr.versions['1'], 'version 1 & 2')
-assert.ok(otr.versions['2'], 'version 1 & 2')
+    assert.equal('', P.parseMsg(otr, MSGFRAG1), 'Message fragment 1.')
+    assert.equal('', P.parseMsg(otr, MSGFRAG2), 'Message fragment 2.')
+    var three = P.parseMsg(otr, MSGFRAG3)
+    assert.equal(three, P.parseMsg(otr, MSGFRAG), 'Message fragment.')
+    assert.equal('OTR', three, 'Message fragment 3.')
+  });
 
-otr = new OTR()
-P.parseMsg(otr, '?OTRv24x?')
-assert.equal(3, Object.keys(otr.versions).length, 'version 2, 4, x')
-assert.ok(otr.versions['2'], 'version 2, 4, x')
-assert.ok(otr.versions['4'], 'version 2, 4, x')
-assert.ok(otr.versions.x, 'version 2, 4, x')
+  it('should parse otr "Version 1 Only" query message', function(){
+    P.parseMsg(otr, '?OTR?')
+    assert.equal(1, Object.keys(otr.versions).length, 'version 1')
+    assert.ok(otr.versions['1'], 'version 1')
+  });
 
-otr = new OTR()
-P.parseMsg(otr, '?OTR?v24x?')
-assert.equal(4, Object.keys(otr.versions).length, 'version 1, 2, 4, x')
-assert.ok(otr.versions['1'], 'version 1, 2, 4, x')
-assert.ok(otr.versions['2'], 'version 1, 2, 4, x')
-assert.ok(otr.versions['4'], 'version 1, 2, 4, x')
-assert.ok(otr.versions.x, 'version 1, 2, 4, x')
+  it('should parse otr "Version 2 Only" query message', function(){
+    P.parseMsg(otr, '?OTRv2?')
+    assert.equal(1, Object.keys(otr.versions).length, 'version 2')
+    assert.ok(otr.versions['2'], 'version 2')
+  });
 
-otr = new OTR()
-P.parseMsg(otr, '?OTR?v?')
-assert.equal(1, Object.keys(otr.versions).length, 'version 1 ?')
-assert.ok(otr.versions['1'], 'version 1 ?')
+  it('should parse otr "Version 1 & 2" query message', function(){
+    P.parseMsg(otr, '?OTR?v2?')
+    assert.equal(2, Object.keys(otr.versions).length, 'version 1 & 2')
+    assert.ok(otr.versions['1'], 'version 1 & 2')
+    assert.ok(otr.versions['2'], 'version 1 & 2')
+  });
 
-otr = new OTR()
-P.parseMsg(otr, '?OTRv?')
-assert.equal(0, Object.keys(otr.versions).length, 'version bizarre')
+  it('should parse otr "Version 2, 4, x only" query message', function(){
+    P.parseMsg(otr, '?OTRv24x?')
+    assert.equal(3, Object.keys(otr.versions).length, 'version 2, 4, x')
+    assert.ok(otr.versions['2'], 'version 2, 4, x')
+    assert.ok(otr.versions['4'], 'version 2, 4, x')
+    assert.ok(otr.versions.x, 'version 2, 4, x')
+  });
 
-otr = new OTR()
-assert.equal('This is an error.', (P.parseMsg(otr, '?OTR Error:This is an error.')).message, 'Err.')
+
+  it('should parse otr "Version 2, 4, x" query message', function(){
+    P.parseMsg(otr, '?OTR?v24x?')
+    assert.equal(4, Object.keys(otr.versions).length, 'version 1, 2, 4, x')
+    assert.ok(otr.versions['1'], 'version 1, 2, 4, x')
+    assert.ok(otr.versions['2'], 'version 1, 2, 4, x')
+    assert.ok(otr.versions['4'], 'version 1, 2, 4, x')
+    assert.ok(otr.versions.x, 'version 1, 2, 4, x')
+  });
+
+  it('should parse otr "Version 1 Only" query message 2', function(){
+    P.parseMsg(otr, '?OTR?v?')
+    assert.equal(1, Object.keys(otr.versions).length, 'version 1 ?')
+    assert.ok(otr.versions['1'], 'version 1 ?')
+  });
+
+  it('should parse otr bizarre claim query message', function(){
+    P.parseMsg(otr, '?OTRv?')
+    assert.equal(0, Object.keys(otr.versions).length, 'version bizarre')
+  });
+
+  it('should parse otr error message', function(){
+    assert.equal('This is an error.', (P.parseMsg(otr, '?OTR Error:This is an error.')).message, 'Err.')
+  });
+});
