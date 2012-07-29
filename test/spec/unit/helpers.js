3f7b0842c05f2637583a469f3952d00f3bd887aa
William Budington <bill.budington@gmail.com>
2012-07-29 16:58:14 -0700

    merging tests

--- test/spec/unit/helpers.js
+++ test/spec/unit/helpers.js
@@ -1,43 +1,43 @@
 var assert = require('assert')
-  , BigInt = require('../vendor/bigint.js')
-  , HLP = require('../helpers.js')
+  , BigInt = require('../../../vendor/bigint.js')
+  , HLP = require('../../../helpers.js')
 
 
 describe('Helpers', function(){
   var two55, str;
   describe('packData', function(){
     it('should pack empty data correctly', function(){
-      assert.equal('\0\0\0\0', hlp.packData(''), 'Empty pack.')
+      assert.equal('\0\0\0\0', HLP.packData(''), 'Empty pack.')
     });
   });
   
   describe('packMPI', function(){
     it('should pack mpi data correctly', function(){
-      var test = hlp.packMPI(BigInt.str2bigInt('65280', 10))
+      var test = HLP.packMPI(BigInt.str2bigInt('65280', 10))
       assert.equal('\0\0\0\2\xff\0', test, 'They be equal.')
 
-      var test3 = hlp.packMPI(BigInt.str2bigInt('0', 10))
+      var test3 = HLP.packMPI(BigInt.str2bigInt('0', 10))
       assert.equal('\0\0\0\0', test3, 'Zero')
 
 
       two55 = '32317006071311007300714876688669951960444102669715484032130345427524655138867890893197201411522913463688717960921898019494119559150490921095088152386448283120630877367300996091750197750389652106796057638384067568276792218642619756161838094338476170470581645852036305042887575891541065808607552399123930385521914333389668342420684974786564569494856176035326322058077805659331026192708460314150258592864177116725943603718461857357598351152301645904403697613233287231227125684710820209725157101726931323469678542580656697935045997268352998638215525166389437335543602135433229604645318478604952148193555853611059596230656'
       str = '\0\0\1\1\1'
       for (var i = 0; i < 256; i++) str += '\0'
-      assert.equal(str, hlp.packMPI(BigInt.str2bigInt(two55, 10)), 'BigInt')
+      assert.equal(str, HLP.packMPI(BigInt.str2bigInt(two55, 10)), 'BigInt')
     });
   });
 
   describe('readMPI', function(){
     it('should read mpi data correctly', function(){
-      assert.equal('65280', BigInt.bigInt2str(hlp.readMPI('\0\0\0\2\xff\0'), 10), 'Read MPI.')
+      assert.equal('65280', BigInt.bigInt2str(HLP.readMPI('\0\0\0\2\xff\0'), 10), 'Read MPI.')
 
-      assert.equal(two55, BigInt.bigInt2str(hlp.readMPI(str), 10))
+      assert.equal(two55, BigInt.bigInt2str(HLP.readMPI(str), 10))
     });
   });
 
   describe('twotothe', function(){
     it('should exponentiate a BigInt with base two', function(){
-      assert.equal((Math.pow(2, 513)).toString(16), BigInt.bigInt2str(hlp.twotothe(513), 16))
+      assert.equal((Math.pow(2, 513)).toString(16), BigInt.bigInt2str(HLP.twotothe(513), 16))
     });
   });
 });
--- test/spec/unit/parse.js
+++ test/spec/unit/parse.js
@@ -1,6 +1,6 @@
 var assert = require('assert')
   , OTR = function () { this.versions = {} }  // just a constructor
-  , P = require('../parse.js')
+  , P = require('../../../parse.js')
 
 describe('OTR', function(){
   var otr;
@@ -10,42 +10,42 @@ describe('OTR', function(){
   });
 
   it('should detect message fragments', function(){
-    assert.equal('hi', otr.parseMsg('hi'), 'Hi.')
-    assert.equal('hi?OTR', otr.parseMsg('hi?OTR'), 'OTR!')
+    assert.equal('hi', P.parseMsg(otr, 'hi'), 'Hi.')
+    assert.equal('hi?OTR', P.parseMsg(otr, 'hi?OTR'), 'OTR!')
 
     var MSGFRAG1 = '?OTR,1,3,?OTR:AAIKAAAAAQAAAAEAAADAVf3Ei72ZgFeKqWvLMnuVPVCwxktsOZ1QdjeLp6jn62mCVtlY9nS6sRkecpjuLYHRxyTdRu2iEVtSsjZqK55ovZ35SfkOPHeFYa9BIuxWi9djHMVKQ8KOVGAVLibjZ6P8LreDSKtWDv9YQjIEnkwFVGCPfpBq2SX4VTQfJAQXHggR8izKxPvluXUdG9rIPh4cac98++VLdIuFMiEXjUIoTX2rEzunaCLMy0VIfowlRsgsKGrwhCCv7hBWyglbzwz+AAAAAAAAAAQAAAF2SOr,'
     var MSGFRAG2 = '?OTR,2,3,JvPUerB9mtf4bqQDFthfoz/XepysnYuReHHEXKe+BFkaEoMNGiBl4TCLZx72DvmZwKCewWRH1+W66ggrXKw2VdVl+vLsmzxNyWChGLfBTL5/3SUF09BfmCEl03Ckk7htAgyAQcBf90RJznZndv7HwVAi3syupi0sQDdOKNPyObR5FRtqyqudttWmSdmGCGFcZ/fZqxQNsHB8QuYaBiGL7CDusES+wwfn8Q7BGtoJzOPDDx6KyIyox/flPx2DZDJIZrMz9b0V70a9kqKLo/wcGhvHO6coCyMxenBAacLJ1DiI,'
     var MSGFRAG3 = '?OTR,3,3,NLKoYOoJTM7zcxsGnvCxaDZCvsmjx3j8Yc5r3i3ylllCQH2/lpr/xCvXFarGtG7+wts+UqstS9SThLBQ9Ojq4oPsX7HBHKvq19XU3/ChIgWMy+bczc5gpkC/eLAIGfJ0D5DJsl68vMXSmCoFK0HTwzzNa7lnZK4IutYPBNBCv0pWORQqDpskEz96YOGyB8+gtpFgCrkuV1bSB9SRVmEBfDtKPQFhKowAAAAA=.,'
     var MSGFRAG = '?OTR:AAIKAAAAAQAAAAEAAADAVf3Ei72ZgFeKqWvLMnuVPVCwxktsOZ1QdjeLp6jn62mCVtlY9nS6sRkecpjuLYHRxyTdRu2iEVtSsjZqK55ovZ35SfkOPHeFYa9BIuxWi9djHMVKQ8KOVGAVLibjZ6P8LreDSKtWDv9YQjIEnkwFVGCPfpBq2SX4VTQfJAQXHggR8izKxPvluXUdG9rIPh4cac98++VLdIuFMiEXjUIoTX2rEzunaCLMy0VIfowlRsgsKGrwhCCv7hBWyglbzwz+AAAAAAAAAAQAAAF2SOrJvPUerB9mtf4bqQDFthfoz/XepysnYuReHHEXKe+BFkaEoMNGiBl4TCLZx72DvmZwKCewWRH1+W66ggrXKw2VdVl+vLsmzxNyWChGLfBTL5/3SUF09BfmCEl03Ckk7htAgyAQcBf90RJznZndv7HwVAi3syupi0sQDdOKNPyObR5FRtqyqudttWmSdmGCGFcZ/fZqxQNsHB8QuYaBiGL7CDusES+wwfn8Q7BGtoJzOPDDx6KyIyox/flPx2DZDJIZrMz9b0V70a9kqKLo/wcGhvHO6coCyMxenBAacLJ1DiINLKoYOoJTM7zcxsGnvCxaDZCvsmjx3j8Yc5r3i3ylllCQH2/lpr/xCvXFarGtG7+wts+UqstS9SThLBQ9Ojq4oPsX7HBHKvq19XU3/ChIgWMy+bczc5gpkC/eLAIGfJ0D5DJsl68vMXSmCoFK0HTwzzNa7lnZK4IutYPBNBCv0pWORQqDpskEz96YOGyB8+gtpFgCrkuV1bSB9SRVmEBfDtKPQFhKowAAAAA=.'
 
-    assert.equal('', otr.parseMsg(MSGFRAG1), 'Message fragment 1.')
-    assert.equal('', otr.parseMsg(MSGFRAG2), 'Message fragment 2.')
-    var three = otr.parseMsg(MSGFRAG3)
-    assert.equal(three, otr.parseMsg(MSGFRAG), 'Message fragment.')
+    assert.equal('', P.parseMsg(otr, MSGFRAG1), 'Message fragment 1.')
+    assert.equal('', P.parseMsg(otr, MSGFRAG2), 'Message fragment 2.')
+    var three = P.parseMsg(otr, MSGFRAG3)
+    assert.equal(three, P.parseMsg(otr, MSGFRAG), 'Message fragment.')
     assert.equal('OTR', three, 'Message fragment 3.')
   });
 
   it('should parse otr "Version 1 Only" query message', function(){
-    otr.parseMsg('?OTR?')
+    P.parseMsg(otr, '?OTR?')
     assert.equal(1, Object.keys(otr.versions).length, 'version 1')
     assert.ok(otr.versions['1'], 'version 1')
   });
 
   it('should parse otr "Version 2 Only" query message', function(){
-    otr.parseMsg('?OTRv2?')
+    P.parseMsg(otr, '?OTRv2?')
     assert.equal(1, Object.keys(otr.versions).length, 'version 2')
     assert.ok(otr.versions['2'], 'version 2')
   });
 
   it('should parse otr "Version 1 & 2" query message', function(){
-    otr.parseMsg('?OTR?v2?')
+    P.parseMsg(otr, '?OTR?v2?')
     assert.equal(2, Object.keys(otr.versions).length, 'version 1 & 2')
     assert.ok(otr.versions['1'], 'version 1 & 2')
     assert.ok(otr.versions['2'], 'version 1 & 2')
   });
 
   it('should parse otr "Version 2, 4, x only" query message', function(){
-    otr.parseMsg('?OTRv24x?')
+    P.parseMsg(otr, '?OTRv24x?')
     assert.equal(3, Object.keys(otr.versions).length, 'version 2, 4, x')
     assert.ok(otr.versions['2'], 'version 2, 4, x')
     assert.ok(otr.versions['4'], 'version 2, 4, x')
@@ -54,7 +54,7 @@ describe('OTR', function(){
 
 
   it('should parse otr "Version 2, 4, x" query message', function(){
-    otr.parseMsg('?OTR?v24x?')
+    P.parseMsg(otr, '?OTR?v24x?')
     assert.equal(4, Object.keys(otr.versions).length, 'version 1, 2, 4, x')
     assert.ok(otr.versions['1'], 'version 1, 2, 4, x')
     assert.ok(otr.versions['2'], 'version 1, 2, 4, x')
@@ -63,17 +63,17 @@ describe('OTR', function(){
   });
 
   it('should parse otr "Version 1 Only" query message 2', function(){
-    otr.parseMsg('?OTR?v?')
+    P.parseMsg(otr, '?OTR?v?')
     assert.equal(1, Object.keys(otr.versions).length, 'version 1 ?')
     assert.ok(otr.versions['1'], 'version 1 ?')
   });
 
   it('should parse otr bizarre claim query message', function(){
-    otr.parseMsg('?OTRv?')
+    P.parseMsg(otr, '?OTRv?')
     assert.equal(0, Object.keys(otr.versions).length, 'version bizarre')
   });
 
   it('should parse otr error message', function(){
-    assert.equal('This is an error.', (otr.parseMsg('?OTR Error:This is an error.')).message, 'Err.')
+    assert.equal('This is an error.', (P.parseMsg(otr, '?OTR Error:This is an error.')).message, 'Err.')
   });
 });
