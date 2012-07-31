cbf80faed5ab5b76712eacc15ddc1a3dfabdf67a
Arlo Breault <arlolra@gmail.com>
2012-07-30 21:54:28 -0700

    fix sm test

--- test/spec/unit/sm.js
+++ test/spec/unit/sm.js
@@ -1,11 +1,12 @@
 var assert = require('assert')
   , SM = require('../../../sm.js')
 
-describe('SM', function(){
-  it('', function(){
-    var alice = new SM('arlo')
-    var bob = new SM('arlo')
+describe('SM', function () {
 
+  it('', function(){
+    var alice = new SM('arlo', '123', '456', '789')
+    var bob = new SM('arlo', '123', '456', '789')
     alice.initiate(bob.receiveMsg)
-  });
-});
+  })
+
+})
