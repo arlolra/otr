be2bd5a5cebd3afdd48082d59fe9213ba5571480
William Budington <bill.budington@gmail.com>
2012-08-01 02:16:14 -0700

    order matters apparently
    Signed-off-by: Arlo Breault <arlolra@gmail.com>

--- vendor/cryptojs/cryptojs.js
+++ vendor/cryptojs/cryptojs.js
@@ -1,5 +1,6 @@
 var CryptoJS = require('./lib/core.js');
 
+require('./lib/enc-base64.js')(CryptoJS);
 require('./lib/cipher-core.js')(CryptoJS);
 require('./lib/aes.js')(CryptoJS);
 require('./lib/sha1.js')(CryptoJS);
@@ -7,6 +8,5 @@ require('./lib/sha256.js')(CryptoJS);
 require('./lib/hmac.js')(CryptoJS);
 require('./lib/pad-nopadding.js')(CryptoJS);
 require('./lib/mode-ctr.js')(CryptoJS);
-require('./lib/enc-base64.js')(CryptoJS);
 
 module.exports = CryptoJS
