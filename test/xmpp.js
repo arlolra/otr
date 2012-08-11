abcd990d9b4bede46bfa66766b13fa20e8af8932
Arlo Breault <arlolra@gmail.com>
2012-08-11 16:45:37 -0700

    test xmpp

---
+++ test/xmpp.js
@@ -0,0 +1,35 @@
+var xmpp = require('simple-xmpp')
+  , OTR  = require('../otr.js')
+  , keys = require('./spec/unit/data/keys.js')
+
+var from = ''
+
+function ui(msg) {
+	console.log('ui: ' + msg)
+}
+
+function cb(msg) {
+	xmpp.send(from, msg)
+}
+
+var otr = new OTR(keys.userA, ui, cb)
+
+xmpp.on('online', function() {
+  console.log('Yes, I\'m connected!')
+  otr.sendQueryMsg()
+})
+
+xmpp.on('chat', function(from, message) {
+  otr.receiveMsg(message)
+})
+
+xmpp.on('error', function(err) {
+  console.error(err)
+})
+
+xmpp.connect({
+  jid      : '',
+  password : '',
+  host     : 'talk.google.com',
+  port     : 5222
+})
