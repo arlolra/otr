117510f1170b501f8600c8248cc5490f8d0fc038
Arlo Breault <arlolra@gmail.com>
2012-09-24 15:49:13 -0700

    reliably faster

--- vendor/bigint.js
+++ vendor/bigint.js
@@ -2,6 +2,10 @@
 
   var root = this
 
+  var bpe = 0  // bits stored per array element
+  for (bpe = 0; (1 << (bpe + 1)) > (1 << bpe); bpe++) ;  // bpe = number of bits in the mantissa on this platform
+  bpe >>= 1  // bpe = number of bits in one element of the array representing the bigInt
+
   var BigInt = {
       str2bigInt    : str2bigInt
     , bigInt2str    : bigInt2str
@@ -31,6 +35,7 @@
     , divide_       : divide_
     , trim          : trim
     , expand        : expand
+    , bpe           : bpe
   }
 
   var SeedRandom = root.SeedRandom
@@ -244,7 +249,6 @@
   ////////////////////////////////////////////////////////////////////////////////////////
 
   //globals
-  var bpe=0;         //bits stored per array element
   var mask=0;        //AND this with an array element to chop it down to bpe bits
   var radix=mask+1;  //equals 2^bpe.  A single 1 bit to the left of the last bit of mask.
 
@@ -252,8 +256,6 @@
   var digitsStr='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_=!@#$%^&*()[]{}|;:,.<>/?`~ \\\'\"+-';
 
   //initialize the global variables
-  for (bpe=0; (1<<(bpe+1)) > (1<<bpe); bpe++);  //bpe=number of bits in the mantissa on this platform
-  bpe>>=1;                   //bpe=number of bits in one element of the array representing the bigInt
   mask=(1<<bpe)-1;           //AND the mask with an integer to get its bpe least significant bits
   radix=mask+1;              //2^bpe.  a single 1 bit to the left of the first bit of mask
   var one=int2bigInt(1,1,1);     //constant used in powMod_()
