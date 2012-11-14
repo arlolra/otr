bf0a91a602730c5f01e6d6c1b6818ceec6af0f5b
Arlo Breault <arlolra@gmail.com>
2012-11-14 09:46:40 -0800

    questions

--- test/spec/unit/sm.js
+++ test/spec/unit/sm.js
@@ -9,30 +9,29 @@ describe('SM', function () {
 
   var userA, userB
   beforeEach(function () {
-    var cb = function () {}
+    var cb = function (err) { if (err) throw err }
     var io = function (msg) { userA.receiveMsg(msg) }
-    userB = new OTR(keys.userB, cb, io)
-    userA = new OTR(keys.userA, cb, userB.receiveMsg)
+    var opts = { debug: true }
+    userB = new OTR(keys.userB, cb, io, opts)
+    userA = new OTR(keys.userA, cb, userB.receiveMsg, opts)
   })
 
   it('should ensure message state is encrypted for SM', function () {
     userA.uicb = function (err, msg) {
-      assert.equal(true, !!err, 'Plaintext should not SM.')
+      assert.equal(true, !!err, err)
     }
-    userA.sendQueryMsg()
-    userA.msgstate = CONST.MSGSTATE_PLAINTEXT
     userA.smpSecret()
   })
 
   it('should require an secret to initiate SM', function () {
     userA.uicb = function (err, msg) {
-      assert.equal(true, !!err, 'Secret required for SM.')
+      assert.equal(true, !!err, err)
     }
     userA.sendQueryMsg()
     userA.smpSecret()
   })
 
-  it.skip('should verify the SM secret', function () {
+  it.skip('should verify the SM secret', function (done) {
     userA.uicb = function (err, msg) {
       assert.equal(false, !!err, err)
       assert.equal(false, !!msg, msg)
@@ -43,17 +42,17 @@ describe('SM', function () {
     assert.equal(userB.msgstate, CONST.MSGSTATE_ENCRYPTED, 'Encrypted')
     assert.equal(userA.msgstate, CONST.MSGSTATE_ENCRYPTED, 'Encrypted')
 
-    // figure out API for this
-    userA.secret = 'applesAndOranges'
-    userB.secret = 'applesAndOranges'
+    // callback to ask smp question
+    userB.smcb = function (question) {
+      console.log("question")
+      userB.smpSecret('applesAndOranges')
+    }
 
     assert.ok(!userA.trust, 'Trust B? false')
     assert.ok(!userB.trust, 'Trust A? false')
 
-    userA.sm.initiate()
+    userA.smpSecret('applesAndOranges')
 
-    assert.ok(userA.trust, 'Trust B? true')
-    assert.ok(userB.trust, 'Trust A? true')
   })
 
 })
