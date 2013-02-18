28f4a6ffdb6ba879656872b7b8d0e16d12d879b5
Arlo Breault <arlolra@gmail.com>
2013-02-18 12:33:42 -0800

    some smp tests

--- test/spec/unit/sm.js
+++ test/spec/unit/sm.js
@@ -9,66 +9,82 @@ describe('SM', function () {
 
   var userA, userB
   beforeEach(function () {
-    var cb = function (err) { if (err) throw err }
-    var io = function (msg) { userA.receiveMsg(msg) }
-    var opts = {}
-    userB = new OTR(keys.userB, cb, io, opts)
-    userA = new OTR(keys.userA, cb, userB.receiveMsg, opts)
+    userB = new OTR({ priv: keys.userB })
+    userB.on('error', function (err) { assert.ifError(err) })
+    userB.on('io', function (msg) { userA.receiveMsg(msg) })
+    userA = new OTR({ priv: keys.userA })
+    userA.on('io', function (msg) { userB.receiveMsg(msg) })
   })
 
-  it('should ensure message state is encrypted for SM', function () {
-    userA.uicb = function (err, msg) {
+  it('should ensure message state is encrypted for SM', function (done) {
+    userA.on('error', function (err) {
       assert.equal(true, !!err, err)
-    }
+      done()
+    })
     userA.smpSecret()
   })
 
-  it('should require an secret to initiate SM', function () {
-    userA.uicb = function (err, msg) {
+  it('should require an secret to initiate SM', function (done) {
+    userA.on('error', function (err) {
       assert.equal(true, !!err, err)
-    }
+      done()
+    })
+    userA.on('status', function (state) {
+      if (state === CONST.STATUS_AKE_SUCCESS) {
+        userA.smpSecret()
+      }
+    })
     userA.sendQueryMsg()
-    userA.smpSecret()
   })
 
-  it('should verify the SM secret', function () {
-    userA.uicb = function (err, msg) {
-      assert.equal(false, !!err, err)
-      assert.equal(false, !!msg, msg)
-    }
+  it.only('1 should verify the SM secret', function (done) {
+    this.timeout(3500)
+    var both = false
 
-    userA.sendQueryMsg()  // must have AKEd for SM
+    userA.on('ui', function (msg) { assert.equal(false, !!msg, msg) })
+    userA.on('error', function (err) { assert.equal(false, !!err, err) })
 
-    assert.equal(userB.msgstate, CONST.MSGSTATE_ENCRYPTED, 'Encrypted')
-    assert.equal(userA.msgstate, CONST.MSGSTATE_ENCRYPTED, 'Encrypted')
+    userA.on('status', function (state) {
+      if (state === CONST.STATUS_AKE_SUCCESS) {
+        assert.equal(userB.msgstate, CONST.MSGSTATE_ENCRYPTED, 'Encrypted')
+        assert.equal(userA.msgstate, CONST.MSGSTATE_ENCRYPTED, 'Encrypted')
+        assert.ok(!userA.trust, 'Trust B? false')
+        assert.ok(!userB.trust, 'Trust A? false')
+        userA.smpSecret('applesAndOranges')
+      }
+    })
+
+    userA.on('smp', function (type, data) {
+      if (type === 'trust') {
+        assert.ok(userA.trust, 'Trust B? false')
+        if (both) done()
+        else both = true
+      }
+    })
+
+    userB.sendQueryMsg()  // must have AKEd for SM
 
     // callback to ask smp question
     // should be passed in as opts.smcb
-    userB._smcb = function (type, data) {
+    userB.on('smp', function (type, data) {
       switch (type) {
         case 'question':
-          this.smpSecret('applesAndOranges')
+          userB.smpSecret('applesAndOranges')
           break
         case 'trust':
-          assert.ok(this.trust)
+          assert.ok(userB.trust, 'Trust A? false')
+          if (both) done()
+          else both = true
           break
         default:
           throw new Error('should not be here')
       }
-    }
-
-    assert.ok(!userA.trust, 'Trust B? false')
-    assert.ok(!userB.trust, 'Trust A? false')
-
-    userA.smpSecret('applesAndOranges')
-
-    assert.ok(userA.trust, 'Trust B? false')
-    assert.ok(userB.trust, 'Trust A? false')
+    })
 
   })
 
 
-  it('should verify the SM secret failed', function () {
+  it('2 should verify the SM secret failed', function () {
     userA.uicb = function (err, msg) {
       assert.equal(false, !!err, err)
       assert.equal(false, !!msg, msg)
@@ -104,7 +120,7 @@ describe('SM', function () {
 
   })
 
-  it('should verify the SM secret with question', function () {
+  it('3 should verify the SM secret with question', function () {
     userA.uicb = function (err, msg) {
       assert.equal(false, !!err, err)
       assert.equal(false, !!msg, msg)
