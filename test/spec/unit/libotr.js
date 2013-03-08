980906a73f51f896b5e9044c89e44cce87687b05
Arlo Breault <arlolra@gmail.com>
2013-03-08 13:00:58 -0800

    port golang libotr-3.2.0 test

---
+++ test/spec/unit/libotr.js
@@ -0,0 +1,61 @@
+/*global describe it before */
+var assert = require('assert')
+  , spawn = require('child_process').spawn
+  , keys = require('./data/keys.js')
+  , OTR = require('../../../lib/otr.js')
+  , CONST = require('../../../lib/const.js')
+
+// this test is ported from golang
+// https://code.google.com/p/go/source/browse/otr/otr_test.go?repo=crypto
+
+// compile test/libotr_test_helper.c against libotr-3.2.0
+// output as /tmp/a.out
+
+describe('Libotr', function () {
+
+  var buddy
+  before(function () {
+    buddy = new OTR({ priv: keys.userA })
+  })
+
+  it.skip('should ake and talk with libotr', function (done) {
+    this.timeout(10000)
+
+    var cp = spawn('/tmp/a.out')
+
+    buddy.on('io', function (msg) {
+      cp.stdin.write(msg + '\n')
+    })
+
+    buddy.on('ui', function (msg) {
+      if (msg === "test message") done()
+    })
+
+    buddy.on('status', function (state) {
+      if (state === CONST.STATUS_AKE_SUCCESS) {
+        buddy.sendMsg("Go -> libotr test message")
+      }
+    })
+
+    cp.stdout.on('data', function (msg) {
+      buddy.receiveMsg(msg.toString())
+    })
+
+    cp.stderr.on('data', function (err) {
+      err = err.toString()
+
+      if (err === "libotr helper started\n") {
+        buddy.sendQueryMsg()
+        return
+      }
+
+      // console.log(err)
+    })
+
+    cp.on('exit', function (code) {
+      console.log('exited with code: ' + code)
+    })
+
+  })
+
+})
