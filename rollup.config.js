e58728d7a3b8c94c908e5d1c146193c137510986
sualko <klaus@jsxc.org>
2019-03-21 12:23:31 +0100

    bundle also dsa and sm webworker

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,47 @@
-export default {
+import pkg from './package.json'
+
+let now = new Date();
+let banner = `/*!
+  ${pkg.name}.js v${pkg.version} - ${now.toDateString()}
+  (c) ${now.getFullYear()} - ${pkg.author}
+  Freely distributed under the ${pkg.license} license.
+
+  This file is concatenated for the browser.
+  Please see: ${pkg.homepage}
+*/\n\n`;
+
+export default [{
   input: 'index.js',
   external: ['crypto'],
   output: {
-    file: 'bundle.js',
+    file: 'build/otr.js',
     format: 'umd',
     name: 'arlolra',
+    banner,
     globals: {
       crypto: 'crypto'
     }
   }
-}
+}, {
+  input: 'lib/dsa-webworker.js',
+  external: ['crypto'],
+  output: {
+    file: 'build/dsa-webworker.js',
+    format: 'umd',
+    banner,
+    globals: {
+      crypto: 'crypto'
+    }
+  }
+}, {
+  input: 'lib/sm-webworker.js',
+  external: ['crypto'],
+  output: {
+    file: 'build/sm-webworker.js',
+    format: 'umd',
+    banner,
+    globals: {
+      crypto: 'crypto'
+    }
+  }
+}]
